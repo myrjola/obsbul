33524ffd0c3889f0c8de36797f9fa8b6bb8c9564
Martin Yrjölä <martin.yrjola@gmail.com>
2010-10-30 17:12:25 +0300

    Const correct EntityFactory.

--- src/gamefw/entityfactory.h
+++ src/gamefw/entityfactory.h
@@ -9,49 +9,10 @@
 #define TIXML_USE_STL
 #include <tinyxml.h>
 
-struct _GLMmodel;
-typedef _GLMmodel GLMmodel;
+typedef struct _GLMmodel GLMmodel;
 
-/**
- * @brief Vertex representation.
- *
- * GPU:s like 128-bit (4 floats) aligned buffers.
- */
-typedef struct {
-    /// Vertex position.
-    GLfloat position[4];
-    /// Vertex surface normal.
-    GLfloat normal[4];
-    /// Vertex uv texture coordinate.
-    GLfloat texcoord[2];
-    /// Vertex material index.
-    GLuint material_idx;
-} t_vertex;
+typedef struct _vertex t_vertex;
 
-/**
- * @brief Extra buffers for the vertex representation.
- */
-typedef struct {
-    /// Vertex surface tangent.
-    GLfloat tangent[4];
-    /// Vertex surface bitangent.
-    GLfloat bitangent[4];
-} t_vertex_extra;
-
-/**
- * @brief Mesh material properties.
- */
-typedef struct {
-    /// Diffuse color.
-    GLfloat diffuse[4];
-    /// Specular color.
-    GLfloat specular[4];
-    /// Shininess coefficient.
-    GLfloat shininess;
-    /// Padding needed for std140 layout to align properly.
-    GLfloat padding[3]; // Needed for std140 layout.
-} t_material;
- 
 namespace gamefw {
 
 /**
@@ -82,18 +43,18 @@ public:
      * @param path The absolute path to the Entity's file.
      * @return Reference to the constructed Entity.
      */
-    Entity createEntity(string path);
+    Entity createEntity(const std::string& path);
 
 private:
-    void loadModel(GLMmodel* path, shared_ptr<RenderJob> renderjob);
+    void loadModel(const GLMmodel& model, shared_ptr< RenderJob > renderjob);
     
     void genVertexBuffers(shared_ptr<RenderJob> renderjob,
-            t_vertex* vertex_buffer, size_t vertex_buffer_length,
-            GLushort* element_buffer, size_t element_buffer_length);
+            const t_vertex* vertex_buffer, size_t vertex_buffer_length,
+            const GLushort* element_buffer, size_t element_buffer_length) const;
 
-    void createMaterials(shared_ptr<RenderJob> renderjob, GLMmodel* model);
+    void createMaterials(shared_ptr< RenderJob > renderjob, const GLMmodel& model) const;
 
-    string makeDefineFromEnum(const char* attrib_name, int index);
+    const std::string makeDefineFromEnum(const char* enum_name, int index) const;
 };
 
 }
