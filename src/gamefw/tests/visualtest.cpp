3e3864e73f4868bc47ea89093cce81c99aba506f
myrjola <martin.yrjola@gmail.com>
2010-11-03 13:44:36 +0200

    Beautify visualtest.
    Added more entities with movement.

--- src/gamefw/tests/visualtest.cpp
+++ src/gamefw/tests/visualtest.cpp
@@ -33,9 +33,11 @@ int main(int argc, char* argv[])
 
     Entity entity = Locator::getFileService().createEntity("flatsmooth");
     Entity entity2 = Locator::getFileService().createEntity("sphere");
+    Entity entity3 = Locator::getFileService().createEntity("flatsmooth");
 
-    entity.m_position = glm::vec3(4.0f, 0.0f, -15.0f);
-	entity2.m_position = glm::vec3(-4.0f, 0.0f, -10.0f);
+    entity.m_position = glm::vec3(5.0f, 0.0f, -15.0f);
+	entity2.m_position = glm::vec3(-4.0f, 4.0f, -10.0f);
+	entity3.m_position = glm::vec3(0.0f, 0.0f, -5.0f);
 
     Renderer* renderer = new Renderer(width, height);
 
@@ -44,6 +46,7 @@ int main(int argc, char* argv[])
     sf::Window* main_window = game->getMainWindow();
     main_window->ShowMouseCursor(false);
 
+	float timer = 0.0f;
     while (true) {
         float time_since_draw = clock.GetElapsedTime();
         if (time_since_draw >= 1.0f/60) {
@@ -51,8 +54,16 @@ int main(int argc, char* argv[])
             if (!game->update()) { // If window closing.
                 break;
             }
+			entity2.m_position.x = 4.0f * glm::sin(timer);
+			entity2.m_position.y = 4.0f * glm::cos(timer);
+			timer += 0.01f;
+			entity.m_orientation.pitch += 0.01f;
+			entity2.m_orientation.roll += 0.03f;
+			entity3.m_orientation.yaw += 0.02f;
+			entity3.m_orientation.pitch += 0.005f;
             renderer->addToRenderQueue(entity);
             renderer->addToRenderQueue(entity2);
+            renderer->addToRenderQueue(entity3);
             renderer->render();
             main_window->Display();
         }
