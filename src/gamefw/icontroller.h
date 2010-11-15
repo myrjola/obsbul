e6da0b01a22f3315b7bcabe3b93a54cc9176c5fa
Martin Yrjölä <martin.yrjola@gmail.com>
2010-11-16 01:35:27 +0200

    Document IController.

--- src/gamefw/icontroller.h
+++ src/gamefw/icontroller.h
@@ -5,11 +5,36 @@
 
 namespace gamefw {
 
+/**
+ * @brief Interface for input processing. Used in the Game class.
+ *
+ * Meant to be inherited. Take a look at DefaultFirstPersonController.
+ **/
 class IController
 {
 public:
+    /**
+     * @brief Input processing on key press.
+     *
+     * @param keyevent The pressed key.
+     * @return void
+     **/
     virtual void keyPressed(sf::Event::KeyEvent& keyevent);
+    
+    /**
+     * @brief Input processing on key release.
+     *
+     * @param keyevent The released key.
+     * @return void
+     **/
     virtual void keyReleased(sf::Event::KeyEvent& keyevent);
+
+    /**
+     * @brief Input processing on mouse movement.
+     *
+     * @param event The caught event.
+     * @return void
+     **/
     virtual void mouseMoved(sf::Event::MouseMoveEvent event);
 
 };
